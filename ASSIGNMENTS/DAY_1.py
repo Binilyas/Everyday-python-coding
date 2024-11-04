95d7e3460554768b8971b550a2ca94fd1ea872b6
Binilyas <Ilyasuabdlrahman@gmail.com>
2024-11-04 07:41:12 -0800

    still on testing

---
+++ ASSIGNMENTS/DAY_1.py
@@ -0,0 +1,8 @@
+print("here is my first assignment")
+first_name = ("abdulrahman")
+sure_name = ("ilyasu")
+full_name =   f"{first_name},{sure_name}"
+print (full_name.title())
+print(full_name.upper())
+print(full_name.lower())
+
--- PROJECT/DAY_1.py
+++
@@ -1,7 +0,0 @@
-print("here is my first assignment")
-
-
-first_name = ("abdulrahman")
-sure_name = ("ilyasu")
-full_name =   f"{first_name},{sure_name}.title()"
-print (full_name)
